74727d32ccdbcff8724989d7b6f9aef02d5c4083
Peppe Pace <peppe.pace@gmail.com>
2019-10-01 02:06:14 +0200

    introdotto script python per la conversione dal file json pubblicato tra gli opendata nel formato geojson corrispondente e aggiornato file geojson

---
+++ opendata-to-geojson.py
@@ -0,0 +1,41 @@
+import json
+import urllib.request
+
+operUrl = urllib.request.urlopen('https://opendata.comune.palermo.it/ws.php?id=1905&fmt=json')
+if(operUrl.getcode()==200):
+    fontanelle = json.loads(operUrl.read())
+
+    geoJsonFeatures = []
+
+    for fontanella in fontanelle:
+        geoJsonItem = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    float(fontanella["longitudine"]),
+                    float(fontanella["latitudine"])
+                ]
+            },
+            "properties": {
+                "name" : fontanella["Nome"],
+                "address" : fontanella["Indirizzo"],
+                "marker-color" : "#0080ff",
+                "marker-size" : "medium",
+                "marker-symbol" : "water"
+            }
+        }
+    
+        geoJsonFeatures.append(geoJsonItem)
+    
+        geoJson = {
+            "type": "FeatureCollection",
+            "features": geoJsonFeatures
+        }
+    
+    with open("fontanelle.json", "w") as w:
+        w.write(json.dumps(geoJson, indent=4))
+        w.close()    
+
+else:
+    print("Error receiving data", operUrl.getcode())
